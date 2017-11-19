b35815c238657ffa0fe22a06b1d2be3dd55a9d33
dschlauderaff <dschlaud@gmail.com>
2017-11-19 15:56:34 -0600

    adds seed data to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+textbooks = Textbook.create([{title: "Biology", price: 25},
+                            {title: "Chemistry", price: 45},
+                            {title: "Algebra", price: 100},
+                            {title: "Geology", price: 250},
+                            {title: "Astronomy", price: 105}])
+
+courses = Course.create([
+    {name: 'Bio 1', enrollment: 500},    
+    {name: 'Chem 1', enrollment: 400},    
+    {name: 'Math 1', enrollment: 300},    
+    {name: 'Science 1', enrollment: 200},    
+    {name: 'Astro 1', enrollment: 100},    
+])
